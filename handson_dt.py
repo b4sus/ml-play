3073e913aafd7947a090dca380688c761e72a45f
b4sus <jurob19@gmail.com>
2021-06-10 20:31:48 +0200

    dec tree fun

--- handson_dt.py
+++ handson_dt.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 from sklearn.base import clone
 from sklearn.datasets import make_moons
 from sklearn.model_selection import GridSearchCV, train_test_split, ShuffleSplit
@@ -12,6 +13,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 gs = GridSearchCV(DecisionTreeClassifier(random_state=42),
                    {"max_leaf_nodes": range(5, 30, 2), "max_depth": range(3, 30, 2)},
+                   # {'max_leaf_nodes': list(range(2, 100)), 'min_samples_split': [2, 3, 4]},
                    cv=5)
 gs.fit(X_train, y_train)
 print(gs.best_estimator_)
@@ -19,18 +21,22 @@ print(gs.best_estimator_)
 y_test_predict = gs.predict(X_test)
 
 print(np.mean(y_test == y_test_predict))
-print(accuracy_score(y_test, y_test_predict))
-
-cls = clone(gs.best_estimator_)
-print(cls)
+print(f"Accuracy with single tree is {accuracy_score(y_test, y_test_predict)}")
 
 scores = []
+trees = []
 for train_index, test_index in ShuffleSplit(1000, train_size=100, random_state=42).split(X_train):
     X_train_split = X_train[train_index, :]
     y_train_split = y_train[train_index]
     cls = clone(gs.best_estimator_)
     cls.fit(X_train_split, y_train_split)
-    y_test_predict = cls.predict(X_test)
-    scores.append(np.mean(y_test == y_test_predict))
+    trees.append(cls)
+    scores.append(accuracy_score(y_test, cls.predict(X_test)))
 
 print(f"mean score from 1000 datasets of size 100 is {np.mean(scores)}")
+
+predictions = np.array([tree.predict(X_test) for tree in trees])
+most_common_predictions = stats.mode(predictions)
+y_test_predict_trees = most_common_predictions.mode[0]
+
+print(f"Accuracy with many trees voting is {accuracy_score(y_test, y_test_predict_trees)}")
