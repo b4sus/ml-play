f107293f8aba07471b7fb78e0971d96b6b0311d0
b4sus <jurob19@gmail.com>
2021-11-23 07:37:21 +0100

    experimenting with voting classifiers

--- handson_rf.py
+++ handson_rf.py
@@ -2,6 +2,12 @@ import pickle
 import os.path
 
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import ShuffleSplit
+from sklearn.svm import LinearSVC, SVC
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
 def open_mnist_or_download_if_missing():
@@ -18,6 +24,48 @@ def open_mnist_or_download_if_missing():
 
 
 if __name__ == "__main__":
+
+    random_state = 65
+
     mnist = open_mnist_or_download_if_missing()
 
-    print(mnist.keys())
+    train_idx, test_idx = next(
+        ShuffleSplit(n_splits=1, train_size=60000, random_state=random_state).split(mnist.data, mnist.target))
+    train_idx, cv_idx = next(
+        ShuffleSplit(n_splits=1, train_size=50000, random_state=random_state).split(mnist.data.iloc[train_idx],
+                                                                                    mnist.target.iloc[train_idx]))
+
+    X_train = mnist.data.iloc[train_idx]
+    y_train = mnist.target.iloc[train_idx]
+
+    X_cv = mnist.data.iloc[cv_idx]
+    y_cv = mnist.target.iloc[cv_idx]
+
+    X_test = mnist.data.iloc[test_idx]
+    y_test = mnist.target.iloc[test_idx]
+
+    svc_clf = SVC(max_iter=100, probability=True, random_state=random_state)
+    linear_svc_clf = LinearSVC(max_iter=100, random_state=random_state)
+    rf_clf = RandomForestClassifier(random_state=random_state)
+    lr_clf = LogisticRegression(random_state=random_state)
+    extra_tree_clf = ExtraTreeClassifier(random_state=random_state)
+    classifiers = [svc_clf, linear_svc_clf, rf_clf, lr_clf, extra_tree_clf]
+
+    for clf in classifiers:
+        print(f"Training {clf}")
+        clf.fit(X_train, y_train)
+
+    print(f"Classifiers:{classifiers}")
+    print(f"Scores: {[clf.score(X_cv, y_cv) for clf in classifiers]}")
+
+    named_estimators = [("svc", svc_clf), ("random_forrest", rf_clf), ("logistic_regression", lr_clf),
+                        ("extra_tree", extra_tree_clf)]
+    hard_voting_clf = VotingClassifier(named_estimators, voting="hard")
+    hard_voting_clf.fit(X_train, y_train)
+    soft_voting_clf = VotingClassifier(named_estimators, voting="soft")
+    soft_voting_clf.fit(X_train, y_train)
+
+    print(f"hard voting classifier score: {hard_voting_clf.score(X_cv, y_cv)}")
+    print(f"Scores: {[clf.score(X_cv, y_cv) for clf in hard_voting_clf.estimators_]}")
+    print(f"soft voting classifier score: {soft_voting_clf.score(X_cv, y_cv)}")
+    print(f"Scores: {[clf.score(X_cv, y_cv) for clf in soft_voting_clf.estimators_]}")
